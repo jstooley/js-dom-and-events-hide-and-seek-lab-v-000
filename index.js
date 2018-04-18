b967111eef315f6cd2792f61e12893a66194663b
Justin Tooley <jstooley823@gmail.com>
2018-04-18 19:24:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ function nestedTarget(){
 function increaseRankBy(n){
   let selected = document.querySelectorAll('.ranked-list li');
   for (let i = 0; i < selected.length ; i++ ){
-    selected[i].innerHTML = (parseInt(selected[i].innerHTMl) + n);
+    selected[i].innerHTML = parseInt(selected[i].innerHTMl) + n;
+  }
+}
+function increaseRankBy(n) {
+  var nodes = document.querySelectorAll('.ranked-list li');
+  for(var i = 0; i < nodes.length; i++) {
+    nodes[i].innerHTML = parseInt(nodes[i].innerHTML) + n;
   }
 }
