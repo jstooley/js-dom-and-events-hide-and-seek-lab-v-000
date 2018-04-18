d187a018a0c354c251bf9df1db9a80e2394e01a8
Justin Tooley <jstooley823@gmail.com>
2018-04-18 19:40:04 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,25 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+function nestedTarget(){
+  return document.querySelector('#nested .target')
+}
+
+function increaseRankBy(n) {
+  let nodes = document.querySelectorAll('.ranked-list');
+  for ( let i = 0; i < nodes.length; i++) {
+    nodes[i].innerHTML = parseInt(nodes[i].innerHTML) + n;
+  }
+}
+
+function deepestChild() {
+  let node = document.getElementById('grand-node')
+  let childNode = node.children[0]
+
+  while (childNode) {
+    node = childNode
+    childNode = node.children[0]
+  }
+
+  return node
+}
