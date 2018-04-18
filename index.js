d4dcb9f600cca03dc85f072d6225c3f47a6cdab6
Justin Tooley <jstooley823@gmail.com>
2018-04-18 19:25:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,9 +10,6 @@ function increaseRankBy(n){
     selected[i].innerHTML = parseInt(selected[i].innerHTMl) + n;
   }
 }
-function increaseRankBy(n) {
-  var nodes = document.querySelectorAll('.ranked-list li');
-  for(var i = 0; i < nodes.length; i++) {
-    nodes[i].innerHTML = parseInt(nodes[i].innerHTML) + n;
-  }
+function deepestChild(){
+  
 }
