e1696222b5673be91df2bbb1d66a33584ee5fbc6
Justin Tooley <jstooley823@gmail.com>
2018-04-18 19:30:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,14 +4,16 @@ function getFirstSelector(selector) {
 function nestedTarget(){
   return document.querySelector('#nested .target')
 }
+
 function increaseRankBy(n){
   let selected = document.querySelectorAll('.ranked-list li');
   for (let i = 0; i < selected.length ; i++ ){
     selected[i].innerHTML = parseInt(selected[i].innerHTMl) + n;
   }
 }
+
 function deepestChild() {
-  let node = document.getElementById('#grand-node')
+  let node = document.getElementById('grand-node')
   let nextNode = node.children[0]
 
   while (nextNode) {
