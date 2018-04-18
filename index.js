235759167f5bd84065fd59f0ece98fc19e808e00
Justin Tooley <jstooley823@gmail.com>
2018-04-18 19:33:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function nestedTarget(){
 
 function increaseRankBy(n){
   let lis = document.querySelectorAll('ul.ranked-list li');
-  for (let i = 0; i < node.length ; i++ ){
+  for (let i = 0; i < lis.length ; i++ ){
     lis[i].innerHTML = parseInt(lis[i].innerHTMl) + n;
   }
 }
